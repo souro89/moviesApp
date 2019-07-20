366d06fc125a633ac9f18c4cb0690f90461904ac
souro89 <souro89@gmail.com>
2019-07-20 20:03:48 +0400

    Styled Modal for Login Button

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -3,12 +3,26 @@ import "./Header.css";
 import Button from "@material-ui/core/Button";
 import logo from "../../assets/logo.svg";
 import Modal from "react-modal";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
+};
 
 class Header extends Component {
   constructor() {
     super();
     this.state = {
-      modalIsOpen: false
+      modalIsOpen: false,
+      value: 0
     };
   }
 
@@ -18,6 +32,11 @@ class Header extends Component {
 
   closeModalHandler = () => {
     this.setState({ modalIsOpen: false });
+  };
+
+  tabChangeHandler = (event, value) => {
+    console.log(value);
+    this.setState({ value });
   };
 
   render() {
@@ -40,7 +59,13 @@ class Header extends Component {
           isOpen={this.state.modalIsOpen}
           contentLabel="Login"
           onRequestClose={this.closeModalHandler}
-        />
+          style={customStyles}
+        >
+          <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+            <Tab label="Login" />
+            <Tab label="Register" />
+          </Tabs>
+        </Modal>
       </div>
     );
   }
