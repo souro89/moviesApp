b2162dc61876cde53a9dbc475fe771a5ff695e6e
souro89 <souro89@gmail.com>
2019-07-24 22:00:12 +0400

    Back to Movie Details

--- src/screens/BookShow/BookShow.js
+++ src/screens/BookShow/BookShow.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Header from "../../common/Header/Header";
+import Typography from "@material-ui/core/Typography";
+import "./BookShow.css";
+import Home from "../Home/Home";
 
 class BookShow extends Component {
+  backToDetailsHandler = () => {
+    ReactDOM.render(<Home />, document.getElementById("root"));
+  };
   render() {
-    return <div>BookShow</div>;
+    return (
+      <div>
+        <Header />
+        <div className="bookShow">
+          <Typography className="back" onClick={this.backToDetailsHandler}>
+            &#60; Back to Movie Details
+          </Typography>
+        </div>
+      </div>
+    );
   }
 }
 
